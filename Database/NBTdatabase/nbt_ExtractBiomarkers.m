2edd5c10e6d17d0c29fb16ace8c5a0c8063baa09
simon-shlomo <poil.simonshlomo@gmail.com>
2014-06-03 01:13:41 +0200

    removed "temporary adjustment" nbt_ExtractBiomarkers

--- Database/NBTdatabase/nbt_ExtractBiomarkers.m
+++ Database/NBTdatabase/nbt_ExtractBiomarkers.m
@@ -17,17 +17,11 @@ else
     s = whos;
       counter=1;
       
-% temporary adjustement
 for ii=1:length(s)
-    if(strcmp(superclasses(s(ii).class),'nbt_Biomarker')) & ~strcmp(s(ii).class,'nbt_questionnaire')
+    if(isa(s(ii).class,'nbt_Biomarker'))
         BiomarkerObjects = [BiomarkerObjects, s(ii).name];
         Biomarkers{counter}=eval([s( ii ).name,'.Biomarkers']);
         counter=counter+1;
-    elseif (strcmp(superclasses(s(ii).class),'nbt_Biomarker')) & strcmp(s(ii).class,'nbt_questionnaire')
-        BiomarkerObjects = [BiomarkerObjects, s(ii).name];
-        Biomarkers{counter}={'Answers'};
-        counter=counter+1;
-        
     end
 end
 end
