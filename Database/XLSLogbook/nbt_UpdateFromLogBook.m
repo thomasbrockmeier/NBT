8b3bec8c6928dc2dd60a51e5581a1cc33ab153eb
Simon-Shlomo Poil <poil.simonshlomo@gmail.com>
2014-06-12 16:15:14 +0200

    ready to update Info Object from Logbook

---
+++ Database/XLSLogbook/nbt_UpdateFromLogBook.m
@@ -0,0 +1,18 @@
+function nbt_UpdateFromLogBook(Signal, SignalInfo, SignalPath,filenameLogBook,SignalName)
+%Update SignalInfo with info from XLSLogbook
+
+%Read LogBook - we assume the projectID is ok for now
+[d,d,LogBookData] = xlsread(filenameLogBook);
+for i=3:2:size(LogBookData,2)
+    if(str2num(LogBookData{13,i}(2:end)) == SignalInfo.subjectID)
+       %ok we found the subject - read info
+       SignalInfo.researcherID = LogBookData{6,i};
+       SignalInfo.subject_age = LogBookData{15,i};
+       SignalInfo.subject_gender = LogBookData{14 ,i};
+       SignalInfo.subject_handedness = LogBookData{16,i};
+       SignalInfo.subject_medication = LogBookData{19,i};
+    end
+end
+
+nbt_SaveSignal([],SignalInfo,SignalPath,1,SignalName);
+end
