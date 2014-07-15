95283790ef283ed7f6473780403907a489aa5424
simon-shlomo <poil.simonshlomo@gmail.com>
2014-07-15 19:02:44 +0200

    beginning of dataContainer, correcting error in expandCell

--- Database/NBTelements/nbt_expandCell.m
+++ Database/NBTelements/nbt_expandCell.m
@@ -1,15 +1,27 @@
 function dataout=nbt_expandCell(celldata)
 
 [Csize1 Csize2] = size(celldata);
-    if(Csize1 >= Csize2)
-        dataout = nan(Csize1,1);
+if(Csize1 >= Csize2)
+    dataout = nan(Csize1,1);
+    try
         for counter=1:Csize1
             dataout(counter) = str2num(celldata{counter,1});
         end
-    else
-        dataout = nan(Csize2,1);
+    catch
+        for counter=1:Csize1
+            dataout(counter) = (celldata{counter,1});
+        end
+    end
+else
+    dataout = nan(Csize2,1);
+    try
         for counter=1:Csize2
             dataout(counter) = str2num(celldata{1,counter});
         end
+    catch
+        for counter=1:Csize1
+            dataout(counter) = (celldata{counter,1});
+        end
     end
+end
 end
