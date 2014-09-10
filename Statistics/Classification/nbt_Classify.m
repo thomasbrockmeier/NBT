8347e616a78622b86a2f725977af727c2264fabb
simon-shlomo <poil.simonshlomo@gmail.com>
2014-09-10 18:14:18 +0200

    Improved display of classification results

--- Statistics/Classification/nbt_Classify.m
+++ Statistics/Classification/nbt_Classify.m
@@ -179,31 +179,41 @@ DataReal2 = DataMatrix(:, Outcome == 1);
 DataPred1 = DataMatrix(:, pp < 0.5);
 DataPred2 = DataMatrix(:, pp >= 0.5);
 
+figure('Name','Classification performance')
+
 % plot dot plot of pp values for the full Data matrix
-nbt_DotPlot(figure,0.1,0.025,0,@median,{'Group 1';'Group 2'; 'Probability'},'',pp1',1:length(pp1),1,pp2',1:length(pp2),1);
+nbt_DotPlot(subplot(2,4,1),0.1,0.025,0,@median,{'Group 1';'Group 2'; 'Probability'},'',pp1',1:length(pp1),1,pp2',1:length(pp2),1);
+set(gca,'YLim',[0 1])
 % plot dot plot of Data values - real and classified
 %first real
-nbt_DotPlot(figure,0.1,0.025,0,@median,{'Group 1';'Group 2'; 'Biomarker values'},'',DataReal1,1:size(DataReal1,2),1:size(DataReal1,1),DataReal2,1:size(DataReal2,2),1:size(DataReal2,1));
+nbt_DotPlot(subplot(2,4,2),0.1,0.025,0,@median,{'Group 1';'Group 2'; 'Biomarker values'},'',DataReal1,1:size(DataReal1,2),1:size(DataReal1,1),DataReal2,1:size(DataReal2,2),1:size(DataReal2,1));
 % then predicted
-nbt_DotPlot(figure,0.1,0.025,0,@median,{'Group 1';'Group 2'; 'Predicted groups: Biomarker values'},'',DataPred1,1:size(DataPred1,2),1:size(DataPred1,1),DataPred2,1:size(DataPred2,2),1:size(DataPred2,1));
+nbt_DotPlot(subplot(2,4,3),0.1,0.025,0,@median,{'Group 1';'Group 2'; 'Predicted groups: Biomarker values'},'',DataPred1,1:size(DataPred1,2),1:size(DataPred1,1),DataPred2,1:size(DataPred2,2),1:size(DataPred2,1));
 % Plot ROC
-figure; 
+subplot(2,4,4)
 [FPR,TPR] = perfcurve(Outcome,pp,1);
 plot(FPR,TPR)
 xlabel('False positive rate'); ylabel('True positive rate')
 title('ROC')
 
-figure
-boxplot(accuracy)
+subplot(2,4,5)
+boxplot(accuracy/100)
+set(gca,'YLim',[0.5 1])
 title('Accuracy')
-figure
+
+subplot(2,4,6)
 boxplot(SE)
-title('SE')
-figure
+set(gca,'YLim',[0.5 1])
+title('Sensitivity (SE)')
+
+subplot(2,4,7)
 boxplot(SP)
-title('SP')
-figure
+set(gca,'YLim',[0.5 1])
+title('Specificity (SP)')
+
+subplot(2,4,8)
 boxplot(PP)
+set(gca,'YLim',[0.5 1])
 title('Precision (PP)')
 
 
