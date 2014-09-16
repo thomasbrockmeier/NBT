e332710cf6e9bf91b1225bba3565d561526a30fa
simon-shlomo <poil.simonshlomo@gmail.com>
2014-09-17 01:05:18 +0200

    help upgrade

--- Statistics/Classification/nbt_Classify.m
+++ Statistics/Classification/nbt_Classify.m
@@ -1,3 +1,41 @@
+% nbt_Classify - Main classification interface
+% 
+%
+% Usage:
+%   [s,ModelVars,Bioms] = nbt_Classify(BCell,Outcome,s,Type, ChannelsToUse)
+%
+% Inputs:
+%  
+% Outputs:
+
+
+%------------------------------------------------------------------------------------
+% Originally created by Simon-Shlomo Poil (2012), see NBT website for current email address
+%------------------------------------------------------------------------------------
+%
+% ChangeLog - see version control log at NBT website for details.
+%
+% Copyright (C) 2012  Simon-Shlomo Poil  
+%
+% Part of the Neurophysiological Biomarker Toolbox (NBT)
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+% See Readme.txt for additional copyright information.
+% ---------------------------------------------------------------------------------------
+
 function [s,ModelVars,Bioms]=nbt_Classify(BCell,Outcome,s,Type, ChannelsToUse)
 NCrossVals=100;
 %% create DataMatrix from BCell:
