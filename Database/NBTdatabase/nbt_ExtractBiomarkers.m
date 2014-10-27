14106ce13bfa5e0b885f73a372a74ce84822068d
Simon-Shlomo Poil <poil.simonshlomo@nbt-analytics.com>
2014-10-27 15:48:02 +0100

    wrong versio nof nbt_ExtractBio

--- Database/NBTdatabase/nbt_ExtractBiomarkers.m
+++ Database/NBTdatabase/nbt_ExtractBiomarkers.m
@@ -1,63 +1,55 @@
-
-% This function extracts a list of biomarkers in the caller stack
-% SEE ALSO: nbt_ExtractObject
-
-% Copyright (C) 2014  Simon-Shlomo Poil
-%
-% Part of the Neurophysiological Biomarker Toolbox (NBT)
-%
-% This program is free software; you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation; either version 3 of the License, or
-% (at your option) any later version.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-%
-% See Readme.txt for additional copyright information.
-%
-
-% ChangeLog - see version control log for details
-% <date> - Version <#> - <text>
-
-function [BiomarkerObjects,Biomarkers]=nbt_ExtractBiomarkers(s)
+function [BiomarkerObjects,Biomarkers, BiomarkersFullList]=nbt_extractBiomarkers(s)
 BiomarkerObjects = cell(0,0);
-error(nargchk(0,1,nargin))
+narginchk(0,1)
 if(~exist('s','var'))
     s=evalin('caller','whos');
-  counter=1;
-for ii=1:length(s)
-    if(strcmp(superclasses(s(ii).class),'nbt_Biomarker')) 
-        BiomarkerObjects = [BiomarkerObjects, s(ii).name];
-        Biomarkers{counter}=evalin('caller',[s( ii ).name,'.Biomarkers']);
-        counter=counter+1;
+    counter=1;
+    for ii=1:length(s)
+        try
+        Sclass = superclasses(s(ii).class);
+        Sclass = Sclass(1);
+        if(strcmp(Sclass{1,1}(end-8:end),'Biomarker'))
+            BiomarkerObjects = [BiomarkerObjects, s(ii).name];
+            Biomarkers{counter}=evalin('caller',[s( ii ).name,'.biomarkers;']);
+            counter=counter+1;
+        end
+        catch
+        end
     end
-end
-    
 else
     load(s)
     s = whos;
-      counter=1;
-      
-% temporary adjustement
-for ii=1:length(s)
-    if(strcmp(superclasses(s(ii).class),'nbt_Biomarker')) & ~strcmp(s(ii).class,'nbt_questionnaire')
-        BiomarkerObjects = [BiomarkerObjects, s(ii).name];
-        Biomarkers{counter}=eval([s( ii ).name,'.Biomarkers']);
-        counter=counter+1;
-    elseif (strcmp(superclasses(s(ii).class),'nbt_Biomarker')) & strcmp(s(ii).class,'nbt_questionnaire')
-        BiomarkerObjects = [BiomarkerObjects, s(ii).name];
-        Biomarkers{counter}={'Answers'};
-        counter=counter+1;
-        
+    counter=1;
+    
+    % temporary adjustement
+    for ii=1:length(s)
+        try
+            Sclass = superclasses(s(ii).class);
+            Sclass = Sclass(1);
+            if(strcmp(Sclass{1,1}(end-8:end),'Biomarker') & ~strcmp(s(ii).class,'nbt_questionnaire'))
+                BiomarkerObjects    = [BiomarkerObjects, s(ii).name];
+                try
+                    Biomarkers{counter} = eval([s( ii ).name,'.Biomarkers;']);
+                catch
+                    Biomarkers{counter} = eval([s( ii ).name,'.biomarkers;']);
+                end
+                counter=counter+1;
+            end
+        catch
+        end
     end
-end
+    in = 1;
+    for i = 1:length(BiomarkerObjects)
+        for m = 1:length(Biomarkers{1,i})
+            BiomarkersFullList{in,1} = strcat(BiomarkerObjects{i}, strcat( '.', Biomarkers{1,i}{m}));
+            in = in + 1;
+        end
+        if(isempty(Biomarkers{1,i}))
+            BiomarkersFullList{in,1} = BiomarkerObjects{i};
+            in = in + 1;
+        end
+    end
+    
 end
 
 
