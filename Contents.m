aa5a84fdb3b93322a8d1b2d15cd47eb06ea78116
simon-shlomo <poil.simonshlomo@gmail.com>
2014-11-12 23:57:44 +0100

    Now 0.5.1-alpha

--- Contents.m
+++ Contents.m
@@ -1,9 +1,9 @@
 % Neurophysiological Biomarker Toolbox (NBT) 
-% Version NBT v.0.5.0-alpha
+% Version NBT v.0.5.1-alpha
 
 % NBT Toolbox --
 % Use the NBTwiki at http://www.nbtwiki.net
 
 % Copyright (C) 2008-2014  The developers
-% Main development team: Simon-Shlomo Poil, Sonja Simpraga, Klaus Linkenkaer-Hansen.
+% Main development team: Simon-Shlomo Poil, Sonja Simpraga, Richard Hardstone, Klaus Linkenkaer-Hansen.
 % 
