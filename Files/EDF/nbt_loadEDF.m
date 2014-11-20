fd160741f45b8340c2f735e424ee0d0e1e27de4e
Simon-Shlomo Poil <poil.simonshlomo@nbt-analytics.com>
2014-11-20 17:14:39 +0100

    removed project specific stuff in nbt_loadEDF

--- Files/EDF/nbt_loadEDF.m
+++ Files/EDF/nbt_loadEDF.m
@@ -2,9 +2,9 @@ function EEG = nbt_loadEDF(filewithpath)
 
 EEG = pop_biosig(filewithpath);
 %temp for specific import
-EEG.data = EEG.data(1:26,:);
-EEG.nbchan = 26;
-EEG.chanlocs = readlocs('rTMSchanlocs.sfp');
+% EEG.data = EEG.data(1:26,:);
+% EEG.nbchan = 26;
+% EEG.chanlocs = readlocs('rTMSchanlocs.sfp');
 %temp end
 
 end
