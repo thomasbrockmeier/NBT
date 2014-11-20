c76642faf58504067c8b2d8db774289a33fccbb5
Simon-Shlomo Poil <poil.simonshlomo@nbt-analytics.com>
2014-11-20 15:24:39 +0100

    updated help

--- Database/NBTdatabase/Info/nbt_importSubjectInfo.m
+++ Database/NBTdatabase/Info/nbt_importSubjectInfo.m
@@ -1,3 +1,39 @@
+% This function imports subject information to the SubjectInfo Object from
+% an XLS file
+% Usage:
+% nbt_importSubjectInfo(infoPath, XLSfilename, subjectIDcolumn, importParameters)
+% 
+% Input parameters:
+%  infoPath         : the path of the info files
+%  XLSfilename      : path and filename of the XLS file to import from
+%  subjectIDcolumn  : column number of the subjectIDs 
+%  importParameters : should be a cell, with 'identifiername; column in XLS
+%                     sheet. I.e., {'subjectAge', 2;'subjectGender', 3} if
+%                     the age is in column 2 and the Gender in column 3
+
+%--------------------------------------------------------------------------
+% Copyright (C) 2014 Simon-Shlomo Poil
+%
+% Part of the Neurophysiological Biomarker Toolbox (NBT)
+%
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 3 of the License, or
+% (at your option) any later version.
+%
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with this program; if not, write to the Free Software
+% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+%
+% See Readme.txt for additional copyright information.
+%-------------------------------------------------------------------------
+
+
 function nbt_importSubjectInfo(infoPath, XLSfilename, subjectIDcolumn, importParameters)
 %import XLS file
 [dummy,dummy,rawXLS] = xlsread(XLSfilename);
