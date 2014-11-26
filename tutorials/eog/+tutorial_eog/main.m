5a54e36f4a8d2ca5783cb8fb8686c25a15d2807c
simon-shlomo <poil.simonshlomo@gmail.com>
2014-11-26 20:21:29 +0100

    2nd conflict missed

--- tutorials/eog/+tutorial_eog/main.m
+++ tutorials/eog/+tutorial_eog/main.m
@@ -9,10 +9,6 @@ myPipe = tutorial_eog.create_pipeline;
 
 cleanedData = run(myPipe, 'eeglab_data_epochs_ica.set');
 
-<<<<<<< HEAD
-origData = import(physioset.import.eeglab, 'f1_750to810.set');
-=======
 origData = import(physioset.import.eeglab, 'eeglab_data_epochs_ica.set');
->>>>>>> meegpipe/meegpipe
 
 plot(origData, cleanedData);
