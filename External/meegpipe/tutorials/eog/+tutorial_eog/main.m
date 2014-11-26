e947d170cdffbdb97012cde2bf6f939056c92ca9
simon-shlomo <poil.simonshlomo@gmail.com>
2014-11-26 20:17:44 +0100

    meegpipe updated with upstream

--- External/meegpipe/tutorials/eog/+tutorial_eog/main.m
+++ External/meegpipe/tutorials/eog/+tutorial_eog/main.m
@@ -1,12 +1,18 @@
 % Main tutorial script
 
-urlwrite('http://kasku.org/data/meegpipe/eeglab_data_epochs_ica.set', ...
-    'eeglab_data_epochs_ica.set');
+if ~exist('eeglab_data_epochs_ica.set', 'file')
+    urlwrite('http://kasku.org/data/meegpipe/eeglab_data_epochs_ica.set', ...
+        'eeglab_data_epochs_ica.set');
+end
 
 myPipe = tutorial_eog.create_pipeline;
 
 cleanedData = run(myPipe, 'eeglab_data_epochs_ica.set');
 
+<<<<<<< HEAD
 origData = import(physioset.import.eeglab, 'f1_750to810.set');
+=======
+origData = import(physioset.import.eeglab, 'eeglab_data_epochs_ica.set');
+>>>>>>> meegpipe/meegpipe
 
 plot(origData, cleanedData);
